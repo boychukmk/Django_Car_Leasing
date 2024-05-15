e337168990f80eeb587eea19ef5256e334b862e4
Max Boichuk <boychuk.mk@gmail.com>
2024-05-15 13:26:09 +0300

    Innitial commit

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Home page')
+
+def about(request):
+    return HttpResponse('Home page')
